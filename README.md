61266c9c1b6427e8fa502f5c094d5697cc9597bf
Emiliano Errecalde <emiliano@digitalannexe.co.uk>
2015-09-28 10:51:11 +0100

    Added first commit.. Still needs some adjustments

---
+++ README.md
@@ -0,0 +1,35 @@
+Modal Module
+=============
+*** 
+**Developers**: Emiliano Errecalde.
+
+**Created**: 09/2015
+***
+
+**Summary**: Angular directive to create templates based modal windows.
+
+**Version**: 1.0.0
+
+**Angular Version**: 1.2.x
+
+**Used Techonlogies**: Sass, Angular, Javascript.
+
+## How to implement it first time:
+
+### 1. Copy the whole folder into your preferred location inside your project
+e.g: inside app/shared folder
+
+### 2. Wire up the core files and make sure they are correctly included: directive (modal.drv.js) and style (_modal.scss).
+e.g: styles => @import "../../shared/dynamic-modal/modal";
+
+### 3. Add your templates into the 'templates' folder included which is included in the 'dynamic-modal' folder.
+
+### 4. Add a button or any element in which you will have to add the following attributes: 
+- trigger-modal (required): boolean attribute, not needed to assign any value.
+- modal-tpl="{template name}" (required): Template name. It has to be existing in the template folder inside the modal module. 
+- modal-theme="[white|black]" (optional): Can be white or black. If not provided, default theme is white (white background, dark gray text).
+
+Complete example of the button: <button trigger-modal modal-tpl="example.tpl.html" ng-click title="Modal Title" modal-theme>Open Modal</button>
+
+
+
---
+++ modal.mdl.js
@@ -0,0 +1,111 @@
+/* global angular:false */
+/*jshint unused:false */
+
+(function () {
+  'use strict';
+
+  $('body').append('<div dynamic-modal show="modalShown"></div>');
+
+  function Show(){
+    this.data = {
+      visibility: false,
+      tpl: '',
+      theme:''
+    };
+  }
+
+  //Directive trigger modal
+  // @ngInject
+  function ModalTrigger(show, TplPath){
+    var link = function(scope, elem, attrs){
+      //update modal stage and template
+      console.log(attrs);
+      elem.bind('click', function(){
+        console.log(attrs);
+        show.data.visibility = true; 
+        show.data.tpl = attrs.modalTpl;
+        show.data.theme = attrs.modalTheme;
+      });
+    };
+
+    return{
+      restrict: 'A',
+      scope: {
+        modalTpl: '@',
+        modalTheme: '@'
+      },
+      link: link
+    };
+  }
+
+  //Directive modal
+  // @ngInject
+  function ModalDirective(show, $timeout, TplPath){
+
+    var link = function(scope) {
+
+      //show modal template
+      scope.getTplUrl = function() {
+        console.log('tpl', TplPath+show.data.tpl);
+        return TplPath+show.data.tpl;
+      };
+
+      scope.getTheme = function(){
+        return show.data.theme || 'white';
+      };
+      //Set modal stage
+      scope.show = function(){
+        if(show.data.visibility)
+          $('body').addClass('loading');
+          
+        return show.data.visibility;
+      };
+
+      //update modal stage
+      scope.hideModal = function() {
+        $('body').removeClass('loading');
+        show.data.visibility = false;
+        show.data.tpl = '';
+        show.data.theme = '';
+      };
+
+      scope.$on('closeModal', function(event, data) { 
+        scope.hideModal();
+      });
+
+      scope.$on('updateModal', function(event, data) { 
+        show.data.tpl = data.tpl;
+        if(data.theme)
+          show.data.theme = data.theme;
+      }); 
+         
+    };
+
+    return{
+      restrict: 'EA',
+      scope: {
+        show: '='
+      },
+      replace: true, // Replace with the template below
+      link: link,
+      template: '<div class="mod-modal" ng-show="show()">'+
+                  '<div class="modal-wrap" ng-click="hideModal()"></div>'+
+                  '<div class="modal-container {{getTheme()}}">'+
+                    '<div class="modal-close" ng-click="hideModal()"></div>'+
+                    '<div class="modal-tpl grid-container" ng-include="getTplUrl()"></div>'+
+                  '</div>'+
+                '</div>'
+    };
+  }
+
+  /** Dependency Injection Array for minification **/
+  ModalTrigger.$inject = ['show', 'TplPath'];
+  ModalDirective.$inject = ['show', '$timeout', 'TplPath'];
+  
+
+  angular.module('modals',[])
+    .constant('TplPath', 'shared/dynamic-modal/templates/')
+    .directive('dynamicModal', ModalDirective)
+    .directive('triggerModal', ModalTrigger)
+    .service('show', Show);
+  }());
